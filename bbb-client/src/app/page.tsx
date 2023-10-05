e0baa0b6a6e16fb6148ffe7eb651341ab3bee354
Ju Young Yang <106505763+JuYoung-Yang-00@users.noreply.github.com>
2023-10-05 19:43:09 -0400

    locations milestone3

--- bbb-client/src/app/page.tsx
+++ bbb-client/src/app/page.tsx
@@ -4,6 +4,7 @@ import Messenger from "@/components/Messenger";
 import ProductList from "@/components/ProductList/ProductMain";
 import Register from "@/components/user/register"
 import Login from "@/components/user/login"
+import Locations from "@/components/locations/locations"
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       {/* <ProductList /> */}
       {/* <Register /> */}
       {/* <Login /> */}
+      <Locations />
     </div>
   );
 }
