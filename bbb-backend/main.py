cf71d8225e7fa19922dc0dc4c41011c7316f29c7
Ju Young Yang <106505763+JuYoung-Yang-00@users.noreply.github.com>
2023-10-05 18:04:31 -0400

    recovery for milestone3

--- bbb-backend/main.py
+++ bbb-backend/main.py
@@ -1,13 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from flask_pymongo import PyMongo
+import sys
+import os
+from dotenv import load_dotenv, find_dotenv
+
 
 app = Flask(__name__)
+CORS(app)
 
-import sys
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/bbb"
+# app.config["MONGO"]=PyMongo(app)
+
+
+
 print(sys.path)
 
 @app.route('/')
 def index():
-    return "Hello from Flask!"
+    return "Hello World!"
+    
+
 
 if __name__ == '__main__':
     app.run(port=5000)
+    
