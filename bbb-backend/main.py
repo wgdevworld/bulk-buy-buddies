7074e9068b3311b6f862ed617befb0b566927581
Cynthia France <cf219@duke.edu>
2023-11-07 19:23:25 -0500

    delete secure key

--- bbb-backend/main.py
+++ bbb-backend/main.py
@@ -10,7 +10,6 @@ import secrets
 # Flask Configurations
 app = Flask(__name__)
 # app.secret_key = secrets.token_urlsafe(16)
-app.secret_key = 'super_secure_key_yay'
 # app.config.update(SESSION_COOKIE_SAMESITE="None", SESSION_COOKIE_SECURE=True)
 # app.config["SESSION_PERMANENT"] = False
 # app.config["SESSION_TYPE"] = "filesystem"
