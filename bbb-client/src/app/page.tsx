d35d081a8a687a9aadbc0154b40e75fc42499f0d
Cynthia France <cf219@duke.edu>
2023-11-09 22:17:05 -0500

    comment out code

--- bbb-client/src/app/page.tsx
+++ bbb-client/src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
       {/* <ProductList /> */}
       {/* <Register /> */}
       {/* <Logout/> */}
-      <Login />
+      {/* <Login /> */}
       {/* <Locations /> */}
       {/* <ShopperForm /> */}
       {/* <ShopperMatch /> */}
