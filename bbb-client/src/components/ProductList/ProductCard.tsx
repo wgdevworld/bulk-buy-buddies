e60d3fe56b8be5affc6638e75aceb77cec1465e7
Adam <wj61@duke.edu>
2023-12-03 18:12:03 -0500

    pagination of products

--- bbb-client/src/components/ProductList/ProductCard.tsx
+++ bbb-client/src/components/ProductList/ProductCard.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import Image from "next/image";
 import { Product } from "./ProductMain";
-import "./products.css";
 
 function ProductCard({ product }: { product: Product }) {
   return (
-    <li className="product-card" key={product._id}>
-      <Image src={product.src} alt={product.name} width={250} height={250} />
-      <p className="product-card-text" title={product.name}>{product.name}</p>
-      <p>{product.price}</p>
-    </li>
+    <div className="bg-white border rounded-lg overflow-hidden shadow-md transition-transform transform hover:scale-105">
+      <div className="relative aspect-w-1 aspect-h-1">
+        <Image
+          src={product.src}
+          alt={product.name}
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+      <div className="p-4">
+        <h2 className="text-sm font-semibold mb-2">{product.name}</h2>
+        <p className="text-gray-700">${product.price}</p>
+      </div>
+    </div>
   );
 }
 
--- bbb-client/src/components/ProductList/ProductMain.tsx
+++ bbb-client/src/components/ProductList/ProductMain.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
 import "./products.css";
 import constants from "../../../../bbb-shared/constants.json";
+import ReactPaginate from "react-paginate";
 
 export interface Product {
   _id: string;
@@ -16,6 +17,8 @@ function ProductMain() {
   const [maxPrice, setMaxPrice] = useState("");
   const [category, setCategory] = useState("");
   const [location, setLocation] = useState("");
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemsPerPage = 8;
 
   const categories = Object.keys(constants.categories);
   const warehouses = constants.warehouses;
@@ -29,6 +32,21 @@ function ProductMain() {
     return ret;
   };
 
+  const paginate = (
+    array: Product[],
+    currentPage: number,
+    itemsPerPage: number
+  ) => {
+    const startIndex = currentPage * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    return array.slice(startIndex, endIndex);
+  };
+
+  // populate page with all products on rerender
+  useEffect(() => {
+    handleSearch();
+  }, []);
+
   const handleSearch = async () => {
     console.log("searching for", category);
     try {
@@ -38,9 +56,9 @@ function ProductMain() {
         minPrice
       )}&max_price=${encodeURIComponent(
         maxPrice
-      )}&category=${encodeURIComponent(
-        category
-      )}&location=${encodeURIComponent(location)}`;
+      )}&category=${encodeURIComponent(category)}&location=${encodeURIComponent(
+        location
+      )}`;
       const response = await fetch(url);
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
@@ -53,52 +71,95 @@ function ProductMain() {
   };
 
   return (
-    <div>
-      <input
-        type="text"
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-        placeholder="Search for a product..."
-      />
-      <input
-        type="number"
-        value={minPrice}
-        onChange={(e) => setMinPrice(e.target.value)}
-        placeholder="Min price"
-      />
-      <input
-        type="number"
-        value={maxPrice}
-        onChange={(e) => setMaxPrice(e.target.value)}
-        placeholder="Max price"
-      />
-      <select value={category} onChange={(e) => setCategory(e.target.value)}>
-        <option key={""} value={""}>
-          All
-        </option>
-        {categories.map((item) => (
-          <option key={item} value={item}>
-            {item}
-          </option>
-        ))}
-      </select>
-      <select value={location} onChange={(e) => setLocation(e.target.value)}>
-        <option key={""} value={""}>
-          All
-        </option>
-        {Object.entries(warehouses).map(([location, code]) => (
-          <option key={location} value={code}>
-            {handleString(location)}
-          </option>
-        ))}
-      </select>
+    <div className="p-4">
+      <div className="flex mb-6">
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for a product..."
+          className="px-4 py-3 border rounded-lg w-full text-lg"
+        />
+        <button
+          onClick={handleSearch}
+          className="bg-blue-500 text-white px-4 py-2 mt-4 rounded-lg hover:bg-blue-600 w-full md:w-auto ml-4"
+        >
+          Search
+        </button>
+      </div>
 
-      <button onClick={handleSearch}>Search</button>
-      <ul className="product-list">
-        {products.map((product: Product) => (
-          <ProductCard product={product} key={product._id} />
-        ))}
-      </ul>
+      <div className="mb-4">
+        <div className="flex flex-wrap gap-4 mt-4">
+          <input
+            type="number"
+            value={minPrice}
+            onChange={(e) => setMinPrice(e.target.value)}
+            placeholder="Min price"
+            className="px-4 py-2 border rounded-lg w-full md:w-2/5"
+          />
+          <input
+            type="number"
+            value={maxPrice}
+            onChange={(e) => setMaxPrice(e.target.value)}
+            placeholder="Max price"
+            className="px-4 py-2 border rounded-lg w-full md:w-2/5"
+          />
+        </div>
+        <div className="flex flex-wrap gap-4 mt-4">
+          <select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            className="px-4 py-2 border rounded-lg w-full md:w-2/5"
+          >
+            <option key={""} value={""}>
+              All Categories
+            </option>
+            {categories.map((item) => (
+              <option key={item} value={item}>
+                {item}
+              </option>
+            ))}
+          </select>
+          <select
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            className="px-4 py-2 border rounded-lg w-full md:w-2/5"
+          >
+            <option key={""} value={""}>
+              All Locations
+            </option>
+            {Object.entries(warehouses).map(([location, code]) => (
+              <option key={location} value={code}>
+                {handleString(location)}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+      <div className="flex flex-wrap mt-4 -mx-2">
+        {paginate(products, currentPage, itemsPerPage).map(
+          (product: Product) => (
+            <div
+              className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6 xl:w-1/8 p-2"
+              key={product._id}
+            >
+              <ProductCard product={product} />
+            </div>
+          )
+        )}
+      </div>
+
+      <div className="mt-4">
+        <ReactPaginate
+          previousLabel={"Previous"}
+          nextLabel={"Next"}
+          pageCount={Math.ceil(products.length / itemsPerPage)}
+          onPageChange={(page) => setCurrentPage(page.selected)}
+          containerClassName={"pagination flex space-x-2"}
+          activeClassName={"bg-blue-500 text-white px-2 rounded-lg"}
+          pageClassName={"px-2 py-1 rounded-lg hover:bg-blue-200"}
+        />
+      </div>
     </div>
   );
 }
