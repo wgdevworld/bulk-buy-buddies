28d42b709e76f88c8a9c336775451bd54b6ed913
Cynthia France <cf219@duke.edu>
2023-12-09 11:25:55 -0500

    stats view

--- bbb-client/src/components/user/landingPage.tsx
+++ bbb-client/src/components/user/landingPage.tsx
@@ -6,6 +6,7 @@ import { Account } from "@/components/user/account";
 import Logout from "@/components/user/logout";
 import ActiveRequestScroll from "./userRequestComponent/activeRequestScroll";
 import MatchedRequestScroll from "./userRequestComponent/matchedRequestScroll";
+import AmountSavedStat from "./userStatsComponent/amtSaved";
 
 
 function LandingPage() {
