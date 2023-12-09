48a5893b27d1f8bbbbe9c237cd55bee7a3c4ad25
Cynthia France <cf219@duke.edu>
2023-12-09 18:42:54 -0500

    new requests page

---
+++ bbb-client/src/components/user/requests.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import { useRouter } from 'next/navigation';
+import { Account } from "@/components/user/account";
+import Logout from "@/components/user/logout";
+import ActiveRequestScroll from "./userRequestComponent/activeRequestScroll";
+import MatchedRequestScroll from "./userRequestComponent/matchedRequestScroll";
+
+function RequestsPage() {
+
+    return(
+        <div>requests page</div>
+    )
+}
+export default RequestsPage;
