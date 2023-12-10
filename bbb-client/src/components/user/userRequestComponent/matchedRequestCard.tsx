6f2eee2bf092b4bf097fc6d4c3550b06ab71e1fe
Jason Lee <choonghwanlee@Jasons-MacBook-Pro.local>
2023-12-09 23:08:29 -0500

    more changes

--- bbb-client/src/components/user/userRequestComponent/matchedRequestCard.tsx
+++ bbb-client/src/components/user/userRequestComponent/matchedRequestCard.tsx
@@ -18,43 +18,50 @@ export interface MatchedRequest {
 }
 
 function MatchedRequestCard({
-    _id,
-    userID,
-    category,
-    buddy,
-    buddyID,
-    quantity,
-    location,
-    timeStart,
-    timeEnd,
-    status,
-}: MatchedRequest) {
+    // _id,
+    // userID,
+    // category,
+    // buddy,
+    // buddyID,
+    // quantity,
+    // location,
+    // timeStart,
+    // timeEnd,
+    // status,
+    request, 
+    setModalVisible,
+}: {
+    request: MatchedRequest;
+    setModalVisible: (visible: boolean) => void;
+}) {
 
     return (
-        <div className="-mt-2 p-2 sm:mt-0 sm:w-full sm:max-w-sm sm:flex-shrink-0">
+        <div className="-mt-2 p-2 sm:mt-0 sm:w-full sm:max-w-sm sm:flex-shrink-0" onClick={() => {
+            setModalVisible(true);
+          }}>
             <div className="rounded-2xl bg-gray-50 py-10 text-left ring-1 ring-inset ring-gray-900/5 sm:flex sm:flex-col sm:py-8 sm:justify-start">
                 <div className=" max-w-none px-8">
-                    <p className="text-2xl font-bold tracking-tight text-gray-900 truncate"> {category} </p>
+                    <p className="text-2xl font-bold tracking-tight text-gray-900 truncate"> {request.category} </p>
                     <p>
                     <span className="inline-flex items-baseline">
                         <img src="../images/buddy.png" alt="" className="self-center w-5 h-5 mx-1" />
                         <span className="break-normal text-base font-medium ml-1 text-gray-600"> Buddy: </span>
-                        <span className="text-base font-normal ml-1 text-gray-600"> {buddy} </span>
+                        <span className="text-base font-normal ml-1 text-gray-600"> {request.buddy} </span>
                     </span> 
                     </p>
                     <p className="mt-3">
                         <p>
                             <span className="break-normal text-base font-medium text-gray-600"> Quantity: </span>
-                            <span className="text-base font-normal ml-1 text-gray-600">{quantity} </span>
+                            <span className="text-base font-normal ml-1 text-gray-600">{request.quantity} </span>
                         </p>
                         <p>
                             <span className="break-normal text-base font-medium text-gray-600"> Location: </span>
-                            <span className="text-base font-normal ml-1 text-gray-600">{location} </span>
+                            <span className="text-base font-normal ml-1 text-gray-600">{request.location} </span>
                         </p>
                         <p>
                             <span className="break-normal text-base font-medium text-gray-600"> Date Range: </span>
-                            <span className="text-base font-normal ml-1 text-gray-600">{new Date(timeStart).toLocaleString()} 
-                            to {new Date(timeEnd).toLocaleString()} </span>
+                            <span className="text-base font-normal ml-1 text-gray-600">{new Date(request.timeStart).toLocaleString()} 
+                            to {new Date(request.timeEnd).toLocaleString()} </span>
                         </p>
                     </p>
                 </div>
